8a8196587044609867806739206ba86add8485c9
MaitaneAzueta <maitane.azuetahe@elorrieta-errekamari.com>
2026-01-28 21:12:26 +0100

    si

--- carrito.php
+++ carrito.php
@@ -24,11 +24,11 @@ if (isset($_SESSION['ultima_sesion_comprada'])) {
 
 $datosCompra = null;
 if ($idSesion !== null) {
-    $sql = "SELECT p.NomPelicula, s.NomSala, ses.FecHoraIni
-            FROM sesion ses
-            INNER JOIN pelicula p ON p.IDPelicula = ses.IDPelicula
-            INNER JOIN sala s ON ses.IDSala = s.IDSala
-            WHERE ses.IDSesion = $idSesion";
+    $sql = "SELECT p.NomPelicula, ses.Precio, s.NomSala, ses.FecHoraIni, p.Portada
+        FROM sesion ses
+        INNER JOIN pelicula p ON p.IDPelicula = ses.IDPelicula
+        INNER JOIN sala s ON ses.IDSala = s.IDSala
+        WHERE ses.IDSesion = $idSesion";
 
     $resultado = $conexion->query($sql);
     if ($resultado) {
@@ -38,25 +38,44 @@ if ($idSesion !== null) {
     }
 }
 ?>
+
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css" />
-    <title>Cine Elorrieta</title>
-</head>
-<body>
-    <header>
-        <h1>CINE ELORRIETA</h1>
-        <nav>
-            <a href="index.php">Películas</a>
-            <a href="carrito.php">Carrito</a>
-        </nav>
-    </header>
-    <main>
+    <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="styles.css" />
+            <script type="text/javascript" src="general.js"></script>
+            <title>Cine Elorrieta-Errekamari</title>
+    </head>
+    <body>
+        <header>
+            <div class="contenedor_logo">
+                <a href="index.php"><img class="Logotipo" src="header/image_logo.png" alt="Logotipo de Cine Elorrieta-Errekamari" /></a>
+                <h1>CINE ELORRIETA</h1>
+                <a href="logout.php">Cerrar Sesion</a>
+
+<!----Se mostrara el mensaje "Hola, x-->
+            <?php 
+        if(isset($_SESSION['cliente'])) { 
+            echo "Hola " . $_SESSION['cliente']; 
+        } 
+    ?>
+
+                <a href="login.html"><img class="usuario" src="header/personita.png" alt="Usuario de Cine Elorrieta-Errekamari"/></a>
+            </div>
+            <nav>
+            <div class="contenedor_menu">
+                <a href="index.php"> Películas</a>
+                <a href="carrito.php"> Carrito</a>
+            </div>
+            </nav>
+        </header>
+        <main>
         <h2>Resumen de Entrada</h2>
         <?php if ($datosCompra !== null) { ?>
             <section class="detalle-carrito">
+                <img src="<?php echo $peli['Portada']; ?>" alt="Portada de peliculas" />
                 <p><strong>Usuario:</strong> <?php echo isset($_SESSION['cliente']) ? $_SESSION['cliente'] : 'Julio'; ?></p>
                 <p><strong>Cine:</strong> Cine Elorrieta-Errekamari</p>
                 <p><strong>Película:</strong> <?php echo $datosCompra['NomPelicula']; ?></p>
@@ -69,5 +88,18 @@ if ($idSesion !== null) {
             <a href="index.php">Volver a la cartelera</a>
         <?php } ?>
     </main>
-</body>
+    <footer>
+            <div class="contenedor_footer">
+                <img class="creative_commons" src="footer/cc.png" alt="Imagen de Creative Commons"/>
+                <div class="texto_contenedor_footer">
+                    <p>© 2026 Elorrieta Cines — Todos los derechos reservados</p>
+                </div>
+                <div class="imagenes_contenedor_footer">
+                    <a href="https://www.facebook.com/"><img class="imagenes_redessociales"src="footer/facebook.png" alt="Imagen del icono de facebook"/></a>
+                    <a href="https://www.instagram.com/"><img class="imagenes_redessociales" src="footer/instagram.png" alt="Imagen del icono de instagram"/></a>
+                    <a href="https://www.x.com/"><img class="imagenes_redessociales" src="footer/twitter.png" alt="Imagen del icono de twitter"/></a>
+                </div>
+            </div>
+        </footer>
+    </body>
 </html>
