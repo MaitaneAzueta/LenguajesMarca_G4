10504b986d1adb039779a38d8eb1270500f215f2
Estibaliz Gutierrez <estibalizzahiragutierrezgo@elorrieta-errekamari.com>
2026-02-03 17:51:37 +0100

    carrito y css de nuevo modificados

--- carrito.php
+++ carrito.php
@@ -61,7 +61,7 @@ if (isset($_SESSION['carrito_sesion'])) {
                 <div class="contenedor_logo">
                     <a href="index.php"><img class="logotipo" src="header/image_logo.png" alt="Logotipo" /></a>
                     <h1>CINE ELORRIETA</h1>
-                    <a class="cerrar_sesion" href="logout.php" onclick="return mensaje_cerrarSesion();">Cerrar Sesion</a>
+                    <a class="cerrar_sesion" href="logout.php">Cerrar Sesion</a>
 <!--Contenedor para el usuario que inicie sesion-->
                         <div class="saludo_usuario">
 <!--Aparece un mensaje de bienvenida para el usuario-->
